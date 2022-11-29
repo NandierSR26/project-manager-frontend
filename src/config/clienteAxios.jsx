1f0664e87771f5fc434e50319822d1309254522e
Nandier Ruiz <nandiersruiz@outlook.com>
2022-11-28 21:34:32 -0500

    url backend updated

--- src/config/clienteAxios.jsx
+++ src/config/clienteAxios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const clienteAxios = axios.create({
-    baseURL: `${import.meta.env.BACKEND_URL}/api`
+    baseURL: `${import.meta.env.BACKEND_URL_v2}/api`
 })
 
 export default clienteAxios;
